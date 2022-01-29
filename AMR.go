cc817aa48a890e899e4ded8a62e99847a784dd09
hodlamr <98183020+hodlamr@users.noreply.github.com>
2022-01-29 22:44:12 +1100

    Add files via upload

---
+++ AMR.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main(){
+
+fmt.Println ("    ::::::::   ")
+fmt.Println ("  ::        ::  ")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::::::::::::::::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("                ")
+fmt.Println ("                ")
+fmt.Println ("::            ::")
+fmt.Println (":::          :::")
+fmt.Println (":  ::      :: ::")
+fmt.Println ("::   ::   ::  ::")
+fmt.Println ("::     ::     ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("                ")
+fmt.Println ("                ")
+fmt.Println (":::::::::::::   ")
+fmt.Println ("::           :: ")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::           :: ")
+fmt.Println ("::::::::::::::  ")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("::            ::")
+fmt.Println ("                ")
+}
+
